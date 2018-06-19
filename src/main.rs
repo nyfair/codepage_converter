847e22f56792bbded05a271a8027641268384ab5
nyfair <nyfair2012@gmail.com>
2018-06-19 16:13:38 +0800

    allow file conversion

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 extern crate encoding;
 extern crate walkdir;
 
-use std::{env, process};
+use std::{env::args, fs::{OpenOptions, rename}, io::{BufReader, BufWriter, prelude::*, stdin}, path::Path, process::exit};
 
 fn showhelp() {
-	println!("Usage: codepage_converter PATH FROM_CODE(gbk) TO_CODE(shift_jis)");
+	println!("Usage: codepage_converter PATH|FILE FROM_CODE(gbk) TO_CODE(shift_jis)");
 	println!("Check https://encoding.spec.whatwg.org/#concept-encoding-get for all valid encoding");
-	process::exit(1);
+	exit(1);
 }
 
 fn main() {
-	let args: Vec<_> = env::args().collect();
+	let args: Vec<_> = args().collect();
 	let mut from_code = "gbk";
 	let mut to_code = "shift-jis";
 	match args.len() {
@@ -21,48 +21,66 @@ fn main() {
 	
 	let from_encoding = encoding::label::encoding_from_whatwg_label(from_code).unwrap();
 	let to_encoding = encoding::label::encoding_from_whatwg_label(to_code).unwrap();
-	let files = walkdir::WalkDir::new(&args[1]).into_iter().filter_map(|e| e.ok());
-	let mut fbuf: Vec<String> = Vec::new();
-	let mut cbuf: Vec<String> = Vec::new();
-	for f in files {
-		let fname = f.path().to_str().unwrap();
-		let hex = from_encoding.encode(fname, encoding::EncoderTrap::Ignore).unwrap();
-		let cname = to_encoding.decode(&hex, encoding::DecoderTrap::Ignore).unwrap();
-		println!("{}\n{}", fname, cname);
-		fbuf.push(fname.to_string());
-		cbuf.push(cname.to_string());
-	}
-	let count = fbuf.len();
-	if count < 1 {
-		println!("Couldn't find any files from given path");
-		showhelp();
-	}
-
-	println!("\n{} files/directories in all, do the conversion?(Yes/No/Manually)", count);
-	let mut pflag = false;
-	let mut key = String::new();
-	std::io::stdin().read_line(&mut key).unwrap();
-	match &*key {
-		"m\n" => {},
-		"y\n" => {
-			pflag = true;
-		},
-		_ => { process::exit(0) }
-	}
-
-	let sep: &[_] = &['\\', '/'];
-	for i in 0..count-1 {
-		let fname = &fbuf[count-1-i];
-		let cname = &cbuf[count-1-i];
-		let findex = fname.rfind(sep).unwrap();
-		let cindex = cname.rfind(sep).unwrap();
-		let mut tname = String::new();
-		tname.push_str(&fname[..findex+1]);
-		tname.push_str(&cname[cindex+1..]);
-		if pflag {
-			std::fs::rename(fname, tname).unwrap();
-		} else {
-			println!("mv {} {}", fname, tname);
+	let path = Path::new(&args[1]);
+	if path.is_dir() {
+		let files = walkdir::WalkDir::new(path).into_iter().filter_map(|e| e.ok());
+		let mut fbuf: Vec<String> = Vec::new();
+		let mut cbuf: Vec<String> = Vec::new();
+		for f in files {
+			let fname = f.path().to_str().unwrap();
+			let hex = from_encoding.encode(fname, encoding::EncoderTrap::Ignore).unwrap();
+			let cname = to_encoding.decode(&hex, encoding::DecoderTrap::Ignore).unwrap();
+			println!("{}\n{}", fname, cname);
+			fbuf.push(fname.to_string());
+			cbuf.push(cname.to_string());
 		}
+		let count = fbuf.len();
+		if count < 1 {
+			println!("Couldn't find any files from given path");
+			showhelp();
+		}
+
+		println!("\n{} files/directories in all, do the conversion?(Yes/No/Manually)", count);
+		let mut pflag = false;
+		let mut key = String::new();
+		stdin().read_line(&mut key).unwrap();
+		match &*key {
+			"m\n" => {},
+			"y\n" => {
+				pflag = true;
+			},
+			_ => { exit(0) }
+		}
+
+		let sep: &[_] = &['\\', '/'];
+		for i in 0..count-1 {
+			let fname = &fbuf[count-1-i];
+			let cname = &cbuf[count-1-i];
+			let findex = fname.rfind(sep).unwrap();
+			let cindex = cname.rfind(sep).unwrap();
+			let mut tname = String::new();
+			tname.push_str(&fname[..findex+1]);
+			tname.push_str(&cname[cindex+1..]);
+			if pflag {
+				rename(fname, tname).unwrap();
+			} else {
+				println!("mv \"{}\" \"{}\"", fname, tname);
+			}
+		}
+		if pflag {
+			rename(&fbuf[0], &cbuf[0]).unwrap();
+		} else {
+			println!("mv \"{}\" \"{}\"", &fbuf[0], &cbuf[0]);
+		}
+	} else {
+		let file = OpenOptions::new().read(true).open(path).unwrap();
+    let mut reader = BufReader::new(file);
+    let mut raw:Vec<u8> = Vec::new();
+    reader.read_to_end(&mut raw).unwrap();
+		let hex = from_encoding.decode(&raw, encoding::DecoderTrap::Ignore).unwrap();
+		let traw = to_encoding.encode(hex.as_str(), encoding::EncoderTrap::Ignore).unwrap();
+		let tfile = OpenOptions::new().write(true).open(path).unwrap();
+    let mut writer = BufWriter::new(tfile);
+		writer.write_all(&traw).unwrap();
 	}
 }
